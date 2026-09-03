a0e4c31d03dc21038f3877d3ace833c6ca2a8c5b
Munikrishna929 <munikrishna6076@gmail.com>
2026-09-03 12:47:07 +0530

    Add initial Python project with greeting function

---
+++ main.py
@@ -0,0 +1,8 @@
+# My first project for Alignerr - Muni from Tirupati
+print("Hello Alignerr - I am Muni from Tirupati")
+print("I am a Python Developer")
+
+def greet():
+    return "I can code in Python"
+
+print(greet())
